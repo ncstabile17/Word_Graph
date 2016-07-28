3a2bfc17dc5164ef5c890a69055b7aa896595551
Nick Stabile <ncstabile17@gmail.com>
2016-07-28 11:00:49 -0400

    Separate files and other
    Separated files and trying to add ability to input multiple files

--- file-handling.js
+++ file-handling.js
@@ -1,109 +1,55 @@
 
-function getGraph() {
-      document.getElementById('numWords').innerText = '';
-
-        var randomColorFactor = function() {
-            return Math.round(Math.random() * 255);
-        };
-        var randomColor = function(opacity) {
-            return 'rgba(' + randomColorFactor() + ',' + randomColorFactor() + ',' + randomColorFactor() + ',' + (opacity || '.3') + ')';
-        };
+function getFiles() {
+      
 
         //get inputFile from user selection and read in as string
-        var input = document.getElementById('inputFile').files[0];
-        var reader = new FileReader();
-        reader.onload = function(){
-        var str = reader.result;
 
-          //chapArray is an array where each element
-          //is an array containing the entire text in that chapter
-          //chapters is an array where each element
-          //is an array containing an array of the words in that chapter
-          var chapArray = str.split(/CHAPTER/);
-          var chapters = new Array(chapArray.length);
-          
-          chapArray.forEach(function (element, index, array) {
-            chapters[index] = element.toLowerCase().replace(/[^a-zA-Z '\n]/g, '').split(/\s+/);
-          })
-          
-          //create the labels for the x-axis
-          var xLabels = [];
-          for (var i = 0; i<chapArray.length; i++)
-          {
-            var current = "Chapter " + i;
-            xLabels.push(current);
+        var fileArray = [];
+        var nameArray = [];
+        var fileInput = document.getElementById('inputFile');
+        var files = fileInput.files;
+        var bookArray = [];
+
+        for (var i = 0; i < files.length; i++) {
+          var currFile = files.item(i);
+          fileArray.push(currFile);
+          var currName = currFile.name;
+          nameArray.push(currName);
+        }
+
+        fileArray.forEach(function(element, index, array) {
+              var reader = new FileReader();
+              reader.onload = function(){
+              
+              var currentFile = reader.result;
+              bookArray.push(currentFile);
+
           }
 
-          
-          //var wordCount = countWords(strArray);
-          //printMaxNoCommon(wordCount.count, wordCount.words);
-
-          //get user input for word to graph
-          var wordInput = document.getElementById('wordInput').value;
-          wordInput = wordInput.toString().toLowerCase().split(/, */);
-
-          
-          var datasets = [];
-          //for each input word, go through each chapter counting 
-          //the occurences of the word and push to count array
-          wordInput.forEach(function(element, index, array) {
-            var wordTotal = 0;
-            var wordData = [];
-            chapters.forEach(function(element2, index2, array2) {
-              var wordCount = countWords(array2[index2]);
-              var idx = wordCount.words.indexOf(element);
-              if (idx != -1) {
-                var numCount = wordCount.count[idx];
-                wordData.push(numCount);
-
-              }
-              else {
-                wordData.push(0);
-              }
-            })
-
-            //count total occurence of word
-            for (var j = 0; j<wordData.length; j++) {
-             var currNum = Number(wordData[j]);
-             wordTotal += currNum;
-            }
-
-            //make new dataset from wordData
-            var newDataset = {
-                label: element,
-                borderColor: randomColor(0.4),
-                pointBorderColor: randomColor(0.7),
-                pointBorderWidth: 1,
-                data: wordData,
-            };
-
-            datasets.push(newDataset);
-            console.log(datasets);
-
-              document.getElementById('numWords').innerText += element + ": " + wordTotal + " appearances\n";
               
-              
+        
 
-          })
+        if (element != null) {
+                document.getElementById('frankenimage').style.height=0;
+                reader.readAsText(element);
+                reader.result;
 
+                }
+        else {
+                document.getElementById('error').innerText = "Please select a text file";
+              }
 
-            makeGraph(xLabels, datasets);
+        })
 
-          
-          
-
+        var fileArrayObject = {contents: fileArray,
+                              bookName: nameArray};
+        
+        return fileArrayObject;
           
         };
-        if (input != null) {
-          document.getElementById('frankenimage').style.height=0;
-          reader.readAsText(input);
 
-          }
 
-        else {
-          document.getElementById('error').innerText = "Please select a text file";
-        }
         
-      };
+        
 
 
